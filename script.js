4fdd849f9841229226666bcd2f366dfa9c9edb65
sunitamutyala7-code <sunitamutyala7@gmail.com>
2026-08-25 10:52:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const SUPABASE_URL = "https://ykqnqdtekbxnevtjjkbd.supabase.co";
+
+const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 function findFriends() {
   document.getElementById("friends").scrollIntoView({
     behavior: "smooth"
